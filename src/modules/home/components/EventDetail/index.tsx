4787a09d604f945055a800322a48504ce24a8de4
chayapol <chayapol.cha@ku.th>
2020-11-02 20:43:33 +0700

    add mock event detail

--- src/modules/home/components/EventDetail/index.tsx
+++ src/modules/home/components/EventDetail/index.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 
 import useI18n from 'core/i18n/hooks/useI18n'
 
-import { EVENT_DETAIL_TITLE } from './locales'
+import { EVENT_DETAIL_TITLE, EVENT_DETAIL } from './locales'
 
 const EventDetail = () => {
-  const I18n = useI18n()
+	const I18n = useI18n()
 
-  return <div>{I18n.t(EVENT_DETAIL_TITLE)}</div>
+	return (
+		<div>
+			{I18n.t(EVENT_DETAIL_TITLE)} <br />
+			{I18n.t(EVENT_DETAIL)}
+		</div>
+	)
 }
 
 export default EventDetail
--- src/modules/home/components/EventDetail/locales.ts
+++ src/modules/home/components/EventDetail/locales.ts
@@ -1,1 +1,19 @@
 export const EVENT_DETAIL_TITLE = ['รายละเอียดงาน', 'Event Detail']
+
+const detailThaiVer = `รอลิเอะ gypsum dolor sit mate, ad prompts feud gait, quid exercise emeritus bis e.
+Usu cu ores quid am, me rides sapper croquet ex. 
+Ed ea clit a elect ram referent,at diode imper diet enc. Me sumo unique argument um no. 
+Ea alien um accustoms quo,mod summon effendi it tied.Amalia id per in minimum facility, 
+quid facet modifier ea ma. Ea alien um accustomsquo, mod summon effendi it tied. Imus cause verier ea, 
+grace commode it tied. Idmes mover elect ram assertion has no. Ea elite ague disco bequeath eons. Miniminterpretations man en. 
+Me sumo unique argument um no. Erato dolor em omit tam quono, per leg ere argument um re.`
+
+const detailEngVer = ` Loren gypsum dolor sit mate, ad prompts feud gait, quid exercise emeritus bis e.
+Usu cu ores quid am, me rides sapper croquet ex. 
+Ed ea clit a elect ram referent,at diode imper diet enc. Me sumo unique argument um no. 
+Ea alien um accustoms quo,mod summon effendi it tied.Amalia id per in minimum facility, 
+quid facet modifier ea ma. Ea alien um accustomsquo, mod summon effendi it tied. Imus cause verier ea, 
+grace commode it tied. Idmes mover elect ram assertion has no. Ea elite ague disco bequeath eons. Miniminterpretations man en. 
+Me sumo unique argument um no. Erato dolor em omit tam quono, per leg ere argument um re.`
+
+export const EVENT_DETAIL = [detailThaiVer, detailEngVer]
