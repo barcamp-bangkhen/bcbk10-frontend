b4b15a724818b5ff2c02936be12283b6ba487624
chayapol <chayapol.cha@ku.th>
2020-11-02 21:08:19 +0700

    add button and logo component

--- src/modules/home/components/RegisterSection/index.tsx
+++ src/modules/home/components/RegisterSection/index.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 import Flex from 'common/components/Flex'
 
-import { Container } from './styled'
+import { Container, RegisButton } from './styled'
 
 const RegisterSection = () => (
-    <Container>
-      <Flex alignItems="center" justifyContent="center">
-        Register Section
-      </Flex>
-    </Container>
-  )
+	<Container>
+		<Flex alignItems="center" justifyContent="center" direction="column">
+			RegisterSection
+			<RegisButton>Register Now</RegisButton>
+		</Flex>
+	</Container>
+)
 
 export default RegisterSection
--- src/modules/home/components/RegisterSection/styled.ts
+++ src/modules/home/components/RegisterSection/styled.ts
@@ -3,6 +3,13 @@ import styled from 'styled-components'
 import { NAV_BAR_HEIGHT } from 'core/components/NavigationBar/styled'
 
 export const Container = styled.div`
-  // height: calc(100vh - ${NAV_BAR_HEIGHT}); optional
-  height: 400px;
+	// height: calc(100vh - ${NAV_BAR_HEIGHT}); optional
+	height: 400px;
+`
+export const RegisButton = styled.button`
+	color: orange;
+	padding: 10px;
+`
+export const BarCampLogo = styled.div`
+	background-image: url($logo-location);
 `
