90f2a940a3aaa613225091be7ddb8ac826129d04
ppipee <ppipee.kj@hotmail.com>
2020-11-06 13:46:42 +0700

    fix: styled and node engine

--- src/modules/home/components/RegisterSection/styled.ts
+++ src/modules/home/components/RegisterSection/styled.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
-import { NAV_BAR_HEIGHT } from 'core/components/NavigationBar/styled'
+import { NAV_BAR_HEIGHT } from 'modules/root/components/NavigationBar/styled'
 
 export const Container = styled.div`
-  // height: calc(100vh - ${NAV_BAR_HEIGHT}); optional
-  height: 400px;
+	// height: calc(100vh - ${NAV_BAR_HEIGHT}); optional
+	height: 400px;
 `
