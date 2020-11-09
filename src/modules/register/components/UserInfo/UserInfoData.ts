6fb48630fd363ea290f594a2fa2488f37240ec4d
Tatpol Samakpong <59835246+nicenicegame@users.noreply.github.com>
2020-11-09 17:08:33 +0700

    Revert "Registration form."

--- src/modules/register/components/UserInfo/UserInfoData.ts
+++
@@ -1,9 +0,0 @@
-export interface UserInfoData {
-    firstName: string
-    lastName: string
-    nickname: string
-    conDisease: string
-    allergicFood: string
-    email: string,
-    phoneNumber: string
-}
