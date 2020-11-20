114acfdc5f92b7099f2863666ca9acf9dbc91ac9
Anusid W <anusid.w@ku.th>
2020-11-21 00:58:21 +0700

    Add smooth scroll

--- src/common/components/icons/FireIcon/index.tsx
+++ src/common/components/icons/FireIcon/index.tsx
@@ -1,16 +1,13 @@
 import styled from 'styled-components'
 
-
-export function topFunction(){
-	document.body.scrollTop = 0;
-	document.documentElement.scrollTop=0;
-	
+export function topFunction() {
+	document.body.scrollIntoView({ behavior: 'smooth' })
 }
 
 const FireIcon = styled.img`
 	content: url('/static/images/Artwork/fire.png');
 	height: 64px;
-	scroll-behavior:smooth;
+	scroll-behavior: smooth;
 	cursor: pointer;
 	@media screen and (max-width: 768px) {
 		height: 42px;
