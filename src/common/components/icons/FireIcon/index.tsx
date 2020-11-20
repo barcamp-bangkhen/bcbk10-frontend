40b495a8d5f532f644e66ee5bc4d3d27b21d41cf
chayapol <chayapol.cha@ku.th>
2020-11-20 16:43:05 +0700

    config fire icon

--- src/common/components/icons/FireIcon/index.tsx
+++ src/common/components/icons/FireIcon/index.tsx
@@ -3,9 +3,7 @@ import styled from 'styled-components'
 const FireIcon = styled.img`
 	content: url('/static/images/Artwork/fire.png');
 	height: 64px;
-	/* bottom:0; */
-	/* text-align:center; */
-	position: relative;
+	/* position: relative; */
 
 	@media screen and (max-width: 768px) {
 		height: 42px;
