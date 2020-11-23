5623c1d76dc5a95ae11af734d05eba96041e6bae
chayapol <chayapol.cha@ku.th>
2020-11-23 17:56:45 +0700

    change component dir

---
+++ src/common/components/Footer/styled.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+import Flex from 'common/components/Flex'
+
+export const FooterContain = styled(Flex)`
+	padding: 15px 40px 15px 40px;
+	height:8vh;
+	bottom: 0;
+	@media screen and (max-width: 768px) {
+		padding: 0px 10px 0px 10px;
+
+
+	}
+`
+export const LicenseContainer = styled.div`
+	width: 12rem;
+	color: gray;
+	@media screen and (max-width: 768px) {
+		width: 5rem;
+		font-size: 12px;
+	}
+`
