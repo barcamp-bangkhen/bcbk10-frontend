93a04920e6a6daf843b8d09900568279a1f114e0
chayapol <chayapol.cha@ku.th>
2020-11-26 19:34:33 +0700

    add fix everythings

---
+++ src/modules/home/components/EventDetail/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+import Text from 'common/components/Text'
+
+export const DetailText = styled(Text)`
+    line-height:2.5;
+    @media screen and (max-width: 768px){
+        line-height:2;
+    }
+`
